d70554173a68e79359d8ba85989abea7809120b6
Lakshadeep Baskar <laksha420@gmail.com>
2020-04-10 23:08:06 +0530

    File ready to push

---
+++ sample.py
@@ -0,0 +1,15 @@
+a=input("Enter ypu name:")
+if a==="":
+    print("Name is empty")
+else:
+    print(a)
+ab=input("Enter ypu name:")
+if b==="":
+    print("Name is empty")
+else:
+    print(b)
+
+
+
+
+o->file created 
